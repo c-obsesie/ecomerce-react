8f4fe75fb42a1baed97183f158d5c420a6132fd4
c-obsesie <cosmin.mihalache95.com>
2020-04-30 13:50:09 +0100

    First commit Message

---
+++ src/homepage.componet.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+import './homepage.styles.scss'
+
+const Homepage=()=>(
+    <div className="homepage">
+        <div className="directory-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="hats">Hats</h1>
+                    <span className="subtitle">Shop Here</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="hats">Hats</h1>
+                    <span className="subtitle">Shop Here</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="hats">Hats</h1>
+                    <span className="subtitle">Shop Here</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="hats">Hats</h1>
+                    <span className="subtitle">Shop Here</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="hats">Hats</h1>
+                    <span className="subtitle">Shop Here</span>
+                </div>
+            </div>
+        </div>
+
+    </div>
+)
+
+export default Homepage;
